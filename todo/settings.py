c0129a0a9e28d4c0cb4d7b5a8dec9d98c2a95234
mmaashraf <37049007+mmaashraf@users.noreply.github.com>
2019-03-20 03:15:03 +0530

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mdb!lxo14y3105zi8$ns5y5+-4dr$3ribx0*lr(g9b_zukca1('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
